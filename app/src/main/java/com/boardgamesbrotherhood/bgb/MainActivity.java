5370c7ffba4f29268b31e1596d98359740be7c54
Ángel Píñar Rivas <anglepiri22@gmail.com>
2021-05-13 16:41:33 +0200

    Added bottom navigation bar

--- app/src/main/java/com/boardgamesbrotherhood/bgb/MainActivity.java
+++ app/src/main/java/com/boardgamesbrotherhood/bgb/MainActivity.java
@@ -1,8 +1,14 @@
 package com.boardgamesbrotherhood.bgb;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar toolbar = getSupportActionBar();
+        toolbar.setTitle(R.string.menu_inicio);
+
+        BottomNavigationView bottom_navigation = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+        bottom_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        //TODO something
+                        toolbar.setTitle(R.string.menu_inicio);
+                        return true;
+                    case R.id.action_categories:
+                        //TODO something
+                        toolbar.setTitle(R.string.menu_categories);
+                        return true;
+                    case R.id.action_establishments:
+                        //TODO something
+                        toolbar.setTitle(R.string.menu_establishments);
+                        return true;
+                    case R.id.action_companies:
+                        //TODO something
+                        toolbar.setTitle(R.string.menu_companies);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
     }
 }
