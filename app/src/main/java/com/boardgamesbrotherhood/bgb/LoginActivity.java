1a7336370a4bef8ef904434e87c1bc2cfa2ac0f5
Ángel Píñar Rivas <anglepiri22@gmail.com>
2021-06-07 17:16:13 +0200

    Incluido registro via email/clave

--- app/src/main/java/com/boardgamesbrotherhood/bgb/LoginActivity.java
+++ app/src/main/java/com/boardgamesbrotherhood/bgb/LoginActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,6 +22,13 @@ public class LoginActivity extends AppCompatActivity {
         toolbar.setTitle(R.string.login);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String registeredUser = getIntent().getExtras().getString("user");
+        if(!registeredUser.isEmpty()){
+            Snackbar.make(getWindow().getDecorView().getRootView(), "¡Gracias por unirte a BGB! Ya puedes iniciar sesión", Snackbar.LENGTH_LONG).show();
+            EditText username = findViewById(R.id.textFieldUsername);
+            username.setText(registeredUser);
+        }
     }
 
     @Override
--- app/src/main/java/com/boardgamesbrotherhood/bgb/RegisterActivity.java
+++ app/src/main/java/com/boardgamesbrotherhood/bgb/RegisterActivity.java
@@ -1,11 +1,20 @@
 package com.boardgamesbrotherhood.bgb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,6 +33,22 @@ public class RegisterActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Button btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText username = findViewById(R.id.textFieldRegisterUsername);
+                EditText password = findViewById(R.id.textFieldRegisterPassword);
+                EditText repeatedPassword = findViewById(R.id.textFieldRegisterRepeatPassword);
+
+                String user = username.getText().toString();
+                String pwd = password.getText().toString();
+                String rptPwd = repeatedPassword.getText().toString();
+
+                register(user, pwd, rptPwd);
+            }
+        });
+
 //        fa.signInWithEmailAndPassword("esdfe@hotmail.com", "contraseña").addOnSuccessListener(new OnSuccessListener<AuthResult>() {
 //              @Override
 //              public void onSuccess(AuthResult authResult) {
@@ -34,6 +59,32 @@ public class RegisterActivity extends AppCompatActivity {
 //
 //        fa.createUserWithEmailAndPassword("test@test.com", "clavetest");
     }
+
+    public void register(String username, String password, String repeatedPassword){
+        if(password.equals(repeatedPassword)){
+            fa.createUserWithEmailAndPassword(username, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        succesfullyRegistered(username);
+                    } else {
+                        Snackbar.make(getWindow().getDecorView().getRootView(), "Ha ocurrido un error al hacer el registro", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            });
+        } else {
+            Snackbar.make(getWindow().getDecorView().getRootView(), "Las claves no coinciden", Snackbar.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void succesfullyRegistered(String username){
+        Intent toLoginActivity = new Intent(this, LoginActivity.class);
+        toLoginActivity.putExtra("user", username);
+        toLoginActivity.setFlags(toLoginActivity.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(toLoginActivity);
+    }
+
 
     @Override
     public boolean onSupportNavigateUp() {
